9ee5dfabdf7addb46a702f8f26148c79671a01bf
deathDREAD31 <krishnendu7k@gmail.com>
2026-09-04 16:24:32 +0600

    Update Polygon Amoy RPC endpoint

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
     amoy: {
       url:
         process.env.POLYGON_AMOY_RPC_URL ||
-        "https://rpc-amoy.polygon.technology",
+        "https://polygon-amoy.api.onfinality.io/public",
       chainId: 80002,
       accounts: process.env.BLOCKCHAIN_PRIVATE_KEY
         ? [process.env.BLOCKCHAIN_PRIVATE_KEY]
